5dc0eb8227b55e4eb8c45713efa3cf790f5657f0
Jonathan Lancar <jonaphin@gmail.com>
2018-04-09 21:58:04 -0700

    add npy files to package

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='facenet',
     version='0.0.1',
     description="Face recognition with Google's FaceNet deep neural network & TensorFlow",
     url='https://github.com/jonaphin/facenet',
-    packages=['facenet'],
+    packages= find_packages(),
+    include_package_data=True,
     license='MIT',
     author='David Sandberg',
     packageAuthor='Jonathan Lancar',
     install_requires=[
-        'tensorflow', 'scipy', 'scikit-learn', 'opencv-python', 
+        'tensorflow', 'scipy', 'scikit-learn', 'opencv-python',
         'h5py', 'matplotlib', 'Pillow', 'requests', 'psutil'
     ]
 )
