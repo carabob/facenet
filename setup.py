aebe6d9a3da3c8175fc41034e337bc184cf0f732
Jonathan Lancar <jonaphin@gmail.com>
2018-04-11 19:25:54 -0700

    package update

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='facenet',
-    version='1.0.0',
-    description="Face recognition with Google's FaceNet deep neural network & TensorFlow",
+    version='1.0.1',
+    description="Face recognition using TensorFlow",
+    long_description="Face recognition with Google's FaceNet deep neural network & TensorFlow",
     url='https://github.com/jonaphin/facenet',
     packages= find_packages(),
+    maintainer='Jonathan Lancar',
+    maintainer_email='jonaphin@gmail.com',
     include_package_data=True,
     license='MIT',
-    author='David Sandberg',
-    packageAuthor='Jonathan Lancar',
     install_requires=[
         'tensorflow', 'scipy', 'scikit-learn', 'opencv-python',
         'h5py', 'matplotlib', 'Pillow', 'requests', 'psutil'
